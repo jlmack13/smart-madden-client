6d458a61c1080009bf2f55ce5ae4a06a6e8d9b55
jlmack13 <jennimacklin@gmail.com>
2018-07-28 16:43:13 -0400

    added coverageList to app.js and gave it dummy data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CoverageList from './containers/coverageList';
 
 class App extends Component {
   render() {
+    const coverages = [
+      {
+        id: 1, 
+        name: 'Cover 1'
+      }, 
+      {
+        id: 2,
+        name: 'Cover 2'
+      }, 
+      {
+        id: 3,
+        name: 'Cover 3'
+      }, 
+      {
+        id: 4,
+        name: 'Cover 4'
+      }
+    ];
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Smart Madden Assistant</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, choose a coverage from the list below.
         </p>
+        <CoverageList coverages={coverages} />
       </div>
     );
   }
--- src/containers/coverageList.js
+++ src/containers/coverageList.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import Coverage from '../components/coverage';
 
 const CoverageList = ({coverages}) => {
 
     //need this to recognize which coverage is being clicked eventually
-    handleClick = () => {
+    const handleClick = () => {
         console.log("You clicked a coverage!")
     }
     return (
         <div className="coverageListContainer">
             <ul className="coverageList">
                 {coverages.map( coverage => 
-                    <Coverage name={coverage.name} onClick={this.handleClick}/>
+                    <Coverage key={coverage.id} name={coverage.name} onClick={this.handleClick}/>
                 )}
             </ul>
         </div>
