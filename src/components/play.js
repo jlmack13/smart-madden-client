e1f08cf9f3d8f841ccce2a9e83a6504daa8750ef
jlmack13 <jennimacklin@gmail.com>
2018-08-01 20:57:45 -0400

    built basic component shell for play

--- src/components/play.js
+++ src/components/play.js
@@ -1,1 +1,14 @@
-//build the component for the play card here
+import React from 'react';
+
+//These are just filler props for now
+const Play = ({name, schemes, formation}) => {
+    return (
+        <div className="play">
+            <p>Formation: {formation}</p>
+            <p>{name}</p>
+            <p>Schemes: {schemes}</p>
+        </div>
+    );
+}
+
+export default Play;
