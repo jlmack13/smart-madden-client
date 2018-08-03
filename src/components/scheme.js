fce8885a3af5ed1e17e6deedd3ad4572c6678417
jlmack13 <jennimacklin@gmail.com>
2018-08-03 19:47:02 -0400

    added basic scheme component

--- src/components/scheme.js
+++ src/components/scheme.js
@@ -1,1 +1,15 @@
 //build the component for the scheme here
+import React from 'react';
+
+const Scheme = ({name, instructions}) => {
+    return (
+        <div className="scheme">
+            <h3>{name}</h3>
+            <ul>
+                {instructions.map(instruction => (
+                    <li>{instruction}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
